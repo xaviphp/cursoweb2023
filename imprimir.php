0283496c6b5f4f2ec92a59adb887fd0a3b2245e8
xaviphp <33201295+xaviphp@users.noreply.github.com>
2023-05-17 17:56:21 +0200

    Update imprimir.php

--- imprimir.php
+++ imprimir.php
@@ -1,21 +1,71 @@
 <?php
 //Codi PHP
+//5.A	Aquesta classe representa una impressora la qual te quatre atributs i una funció la qual verificant els atributs permet imprimir o no
+//Les instruccions necesaries per a mostrar en color negre, seria afegir i concatenar la paraula $color dins de la funcio i despres cridar a la funcio passant-li els atributs com faig mes abaix.
+//Si els atributs de la class impressora fossin private o protected es tindrien que fer els getters i setters corresponents:
+    // Getters y Setters para los atributos protegidos
+    /*
+    public function getTinta() {
+        return $this->tinta;
+    }
+    
+    public function setTinta($tinta) {
+        $this->tinta = $tinta;
+    }
+    
+    public function getPaper() {
+        return $this->paper;
+    }
+    
+    public function setPaper($paper) {
+        $this->paper = $paper;
+    }
+    
+    public function getTamany() {
+        return $this->tamany;
+    }
+    
+    public function setTamany($tamany) {
+        $this->tamany = $tamany;
+    }
+    
+    public function getColor() {
+        return $this->color;
+    }
+    
+    public function setColor($color) {
+        $this->color = $color;
+    }
+}
+*/
+//aixi podriem accedir als atributs desde fora la clase
 class Impressora {
     //Atributs
     public $tinta = true;
     public $paper = true;
     public $tamany = 'DINA4';
     public $color= 'negre';
+    
     //Metodes
-    public function imprimeix($tinta,$paper,$tamany){
+    public function imprimeix($tinta,$paper,$tamany, $color){
         if($tinta && $paper){
-            echo 'Es pot imprimir en ' . $tamany;
+            echo 'Es pot imprimir en ' . $tamany . ' en color ' . $color;
         }
         else {
             echo 'Falta tinta o paper';
         }
+        
+    
     }
+    
+    
+    
 }
+
+$impresora = new Impressora(); //Creamos OBJETO
+$impresora->imprimeix(true, true, 'DINA4', 'negre');
+
+
 $semana = ['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO'];
 sort($semana);
 ?>
