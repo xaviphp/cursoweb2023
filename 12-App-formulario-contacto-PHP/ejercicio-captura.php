d48fc71de4d65a4d17c01f94856e83d4b05e0bad
xaviphp <33201295+xaviphp@users.noreply.github.com>
2023-05-26 16:48:48 +0200

    ejercicio formulario completado

--- 12-App-formulario-contacto-PHP/ejercicio-captura.php
+++ 12-App-formulario-contacto-PHP/ejercicio-captura.php
@@ -5,64 +5,62 @@ require 'errores.php';
 if($_SERVER['REQUEST_METHOD']=='GET' && $enviado==true){
     echo"Estamos usando el metodo " . ($_SERVER['REQUEST_METHOD']) . "<br>";
     if($enviado==true){
-    if(isset($_GET['submit'])){
-        echo"Estamos usando el primer submit<br>";
-    }else{
-        echo"Estamos usando el segundo submit<br>";
+        if(isset($_GET['submit'])){
+            echo"Estamos usando el primer submit<br>";
+        } else{
+            echo"Estamos usando el segundo submit<br>";
+        }
     }
-}
-print_r($_GET);
-    if($_GET){
     // print_r($_GET);
-    $nombre=$_GET['nombre'];
-    $nombre= trim($nombre); //quita espacios
-$nombre=htmlspecialchars($nombre); //Nos convierte los caracteres especiales en entidades html
-$nombre=stripslashes($nombre); // Nos quita \ diagonales para no inyectar código
+    if($_GET){
+        // print_r($_GET);
+        $nombre=$_GET['nombre'];
+        $nombre= trim($nombre); //quita espacios
+        $nombre=htmlspecialchars($nombre); //Nos convierte los caracteres especiales en entidades html
+        $nombre=stripslashes($nombre); // Nos quita \ diagonales para no inyectar código
 
-$correo=$_GET['correo'];
-$correo=filter_var($correo, FILTER_SANITIZE_EMAIL);//Filtro de "sanea" el correo
+        $correo=$_GET['correo'];
+        $correo=filter_var($correo, FILTER_SANITIZE_EMAIL);//Filtro de "sanea" el correo
 
-$telefono=$_GET['telefono'];
+        $telefono=$_GET['telefono'];
 
-$mensaje=$_GET['mensaje'];
-$mensaje= trim($mensaje); //quita espacios
-$mensaje=htmlspecialchars($mensaje); //Nos convierte los caracteres especiales en entidades html
-$mensaje=stripslashes($mensaje); // Nos quita \ diagonales para no inyectar código
+        $mensaje=$_GET['mensaje'];
+        $mensaje= trim($mensaje); //quita espacios
+        $mensaje=htmlspecialchars($mensaje); //Nos convierte los caracteres especiales en entidades html
+        $mensaje=stripslashes($mensaje); // Nos quita \ diagonales para no inyectar código
 
-$telefono=$_GET['telefono'];
-echo"<br>Tu nombre es " . $nombre . ", tu correo es " . $correo . ", tu numero de telefono es el " . $telefono . " y has escrito " . $mensaje . $telefono;
-echo"<br>";
+        echo"<br>Tu nombre es " . $nombre . ", tu correo es " . $correo . ", tu numero de telefono es el " . $telefono . " y has escrito " . $mensaje;
+        echo"<br>";
     }
-}elseif($_SERVER['REQUEST_METHOD']=='POST' && $enviado==true){
-    echo"Estamos usando el metodo " . ($_SERVER['REQUEST_METHOD']) . "<br>";
-    if($enviado==true){
-    if(isset($_POST['submit'])){
-        echo"Estamos usando el primer submit<br>";
-    }else{
-        echo"Estamos usando el segundo submit<br>";
-    }
-}
-    print_r($_POST);
-    if($_POST){
-$nombre=$_POST['nombre'];
-$nombre= trim($nombre); //quita espacios
-$nombre=htmlspecialchars($nombre); //Nos convierte los caracteres especiales en entidades html
-$nombre=stripslashes($nombre); // Nos quita \ diagonales para no inyectar código
+} elseif($_SERVER['REQUEST_METHOD']=='POST' && $enviado==true){
+        echo"Estamos usando el metodo " . ($_SERVER['REQUEST_METHOD']) . "<br>";
+        if($enviado==true){
+            if(isset($_POST['submit'])){
+                echo"Estamos usando el primer submit<br>";
+            }else{
+                echo"Estamos usando el segundo submit<br>";
+            }
+        }
+        // print_r($_POST);
+        if($_POST){
+            $nombre=$_POST['nombre'];
+            $nombre= trim($nombre); //quita espacios
+            $nombre=htmlspecialchars($nombre); //Nos convierte los caracteres especiales en entidades html
+            $nombre=stripslashes($nombre); // Nos quita \ diagonales para no inyectar código
 
-$correo=$_POST['correo'];
-$correo=filter_var($correo, FILTER_SANITIZE_EMAIL);//Filtro de "sanea" el correo
+            $correo=$_POST['correo'];
+            $correo=filter_var($correo, FILTER_SANITIZE_EMAIL);//Filtro de "sanea" el correo
 
-$telefono=$_POST['telefono'];
+            $telefono=$_POST['telefono'];
 
-$mensaje=$_POST['mensaje'];
-$mensaje= trim($mensaje); //quita espacios
-$mensaje=htmlspecialchars($mensaje); //Nos convierte los caracteres especiales en entidades html
-$mensaje=stripslashes($mensaje); // Nos quita \ diagonales para no inyectar código
+            $mensaje=$_POST['mensaje'];
+            $mensaje= trim($mensaje); //quita espacios
+            $mensaje=htmlspecialchars($mensaje); //Nos convierte los caracteres especiales en entidades html
+            $mensaje=stripslashes($mensaje); // Nos quita \ diagonales para no inyectar código
 
-$telefono=$_POST['telefono'];
-echo"<br>Tu nombre es " . $nombre . ", tu correo es " . $correo . ", tu numero de telefono es el " . $telefono . " y has escrito " . $mensaje . $telefono;
-echo"<br>";
-}
+            echo"<br>Tu nombre es " . $nombre . ", tu correo es " . $correo . ", tu numero de telefono es el " . $telefono . " y has escrito " . $mensaje;
+            echo"<br>";
+        }
 }
 
 require 'vista.php';
--- 12-App-formulario-contacto-PHP/vista.php
+++ 12-App-formulario-contacto-PHP/vista.php
@@ -1,43 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Ejercicio Formulario Contacto</title>
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="12.estilos.css">
 </head>
+
 <body>
 	<div class="wrap">
-		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get"><!--aqui cambiamos de get a post-->
-			<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre:" value="<?php if(!$enviado && isset($nombre)) echo $nombre ?>">
-
-			<input type="email" class="form-control" id="correo" name="correo" placeholder="Correo:" value="<?php if(!$enviado && isset($correo)) echo $correo ?>">
-			<input type="number" class="form-control" id="telefono" name="telefono" placeholder="telefono:" value="<?php if(!$enviado && isset($telefono)) echo $telefono ?>">
-			<textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:" value="<?php if(!$enviado && isset($mensaje)) echo $mensaje ?>"></textarea>
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
+			<!--aqui cambiamos de get a post-->
+			<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre:" value="<?php if (!$enviado && isset($nombre))
+				echo $nombre ?>">
+				<input type="email" class="form-control" id="correo" name="correo" placeholder="Correo:" value="<?php if (!$enviado && isset($correo))
+				echo $correo ?>">
+				<input type="number" class="form-control" id="telefono" name="telefono" placeholder="telefono:" value="<?php if (!$enviado && isset($telefono))
+				echo $telefono ?>">
+				<textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"><?php if (!$enviado && isset($mensaje))
+				echo $mensaje ?></textarea>
 			<?php if (!empty($errores)): ?>
 				<div class="alert error">
 					<?php echo $errores; ?>
 				</div>
-			<?php elseif($enviado): ?>
+			<?php elseif ($enviado): ?>
 				<div class="alert success">
 					<p>Enviado Correctamente</p>
 				</div>
 			<?php endif ?>
 			<br>
-            <label for="aceptar-terminos">
-    <input type="checkbox" id="aceptar-terminos" name="aceptar-terminos" required>
-    Acepto los términos y condiciones
-  </label>
+			<label for="aceptar-terminos">
+				<input type="checkbox" id="aceptar-terminos" name="aceptar-terminos" required>
+				Acepto los términos y condiciones
+			</label>
 			<input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
 			<br>
 			<br>
 			<br>
 			<br>
-			
+
 
 			<input type="submit" name="submit2" class="btn btn-primary" value="Enviar Correo">
 
 		</form>
 	</div>
 </body>
+
 </html>
